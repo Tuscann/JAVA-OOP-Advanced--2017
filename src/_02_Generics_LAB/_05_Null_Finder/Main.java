2bf232be810aa9efcb911beecfdb4dd030eaf865
Tuscann <fbinnzhivko@gmail.com>
2017-11-23 00:15:16 +0200

    add generecs exercieses

--- src/_02_Generics_LAB/_05_Null_Finder/Main.java
+++ src/_02_Generics_LAB/_05_Null_Finder/Main.java
@@ -1,9 +1,24 @@
 package _02_Generics_LAB._05_Null_Finder;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        List<Integer> integers = new ArrayList<>();
+        Collections.addAll(integers, 1, 2, null, 2, null);
+
+        List<Integer> IntegerNulls = ListUtils.getNullIndices(integers);
+        System.out.println(IntegerNulls);
+
+        List<Integer> list = new ArrayList<>();
+        list.add(5);
+        list.add(6);
+        list.add(null);
+        list.add(null);
+        list.add(7);
+        System.out.println(ListUtils.getNullIndices(list));
+
     }
 }
